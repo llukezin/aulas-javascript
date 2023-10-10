9520513a6f17bacf4f95e5036847b8538adee977
llukezin <lucassmendonca77@gmail.com>
2023-10-10 16:53:07 -0300

    atividade realizada

--- js/03-condicionais.js
+++ js/03-condicionais.js
@@ -11,12 +11,23 @@ let idade = prompt('Quantos anos você tem?');
 let mensagem; //indefinida/undefined
 
 // verificar a idade e determinar se é menor ou maior
-if( idade >= 18 ){
-    mensagem = "maior";
-} else{
-    mensagem = "menor"; 
+// if( idade >= 18 ){
+//  mensagem = "maior";
+// } else{
+//    mensagem = "menor"; 
+// }
+
+// apresentando os reusltados apos a condicional
+// console.log(`${usuario} é ${mensagem} de idade!`);
+// alert(`${usuario} é ${mensagem} de idade!`);
+
+if( idade >= 60 ){
+    mensagem = "idoso(a)";
+} else if( idade >= 18){
+    mensagem = "adulto(a)";
+} else {
+    mensagem = "menor";
 }
 
-//apresentando os reusltados apos a condicional
-console.log(`${usuario} é ${mensagem} de idade!`);
-alert(`${usuario} é ${mensagem} de idade!`);
+console.log(`${usuario} você tem ${idade} anos e é considerado ${mensagem},`);
+alert(`${usuario} você tem ${idade} anos e é considerado ${mensagem}`)
---
+++ js/exercicio01.js
@@ -0,0 +1,14 @@
+let nota1 = 7;
+let nota2 = 8;
+
+let media = (nota1+nota2) / 2;
+
+if (nota1 >=7 ){
+mensagem = "aprovado";
+} else if (media>=5)  {
+    mensagem = "recuperação";
+} else {
+    mensagem = "reprovado"
+};
+
+console.log(`o aluno esta: ${mensagem} `);
