840f68a6d1e017478f75bf316158c1adb92c4e73
llukezin <lucassmendonca77@gmail.com>
2023-10-11 14:38:45 -0300

    adicionando e alterando

--- js/04-operadores-logicos.js
+++ js/04-operadores-logicos.js
@@ -8,15 +8,22 @@ const limiteDeFaltas = 10;
 let aluno = "Jean";
 let mediaFinal = 8.5;
 let faltas = 8;
-let Resutado;
+let resultado;
 
 /*  Operador lógico && (AND - E) */
-if(mediaFinal >= mediaMinima && faltas < limiteDeFaltas){
-    Resutado = "aprovado";
- } else {
-    Resutado = "reprovado";
- }
+// if(mediaFinal >= mediaMinima && faltas <= limiteDeFaltas){
+//    Resutado = "aprovado";
+// } else {
+//    Resutado = "reprovado";
+// }
+if (mediaFinal >= mediaMinima && faltas <= limiteDeFaltas){
+    resultado = "aprovado";
+} else if (faltas > limiteDeFaltas){
+    resultado = "reprovado por faltas";
+} else {
+    resultado = "reprovado";
+}
 
- console.log(`O aluno ${aluno} está ${Resutado}`);
+ console.log(`O aluno ${aluno} está ${resultado}`);
  console.log(`Média do aluno: ${mediaFinal}`);
  console.log(`Faltas do aluno: ${faltas}`);
