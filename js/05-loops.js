1bc0d8d999580c830f20581c773ce9ba93db1140
llukezin <lucassmendonca77@gmail.com>
2023-10-11 16:20:46 -0300

    mudei algumas coisas

--- js/05-loops.js
+++ js/05-loops.js
@@ -10,8 +10,16 @@ comandos tradicionais:
 
 obs.: normalmente o loop é controlado através de uma variávrl contadora. - */
 
-let contador = 1;
-while(contador <=5 ){
-    console.log(`Valor de contador: ${contador}`);
-    contador++; // ++ incremento
-}
+// let contador = 1;
+// while(contador <=5 ){
+//    console.log(`Valor de contador: ${contador}`);
+//    contador++; // ++ incremento
+// }
+
+// EXEMPLO DO/WHILE (FAÇA/ENQUANTO)
+
+let contador = 10;
+do {
+    console.log(`Contador vale: ${contador}`);
+    contador++;
+} while ( contador <= 3 )
