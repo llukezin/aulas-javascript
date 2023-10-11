47fbad8be9592535fc8c2fab33b1742bd5a6b709
llukezin <lucassmendonca77@gmail.com>
2023-10-11 16:04:09 -0300

    loops exemplo

---
+++ js/05-loops.js
@@ -0,0 +1,17 @@
+/*  Comados de Repetição
+instuções para realizar ações por uma quantidade
+específica de vezes.
+
+comandos tradicionais:
+
+- while     -> ENQUANTO
+- do/while  -> FAÇA/ENQUANTO
+- for       -> PARA 
+
+obs.: normalmente o loop é controlado através de uma variávrl contadora. - */
+
+let contador = 1;
+while(contador <=5 ){
+    console.log(`Valor de contador: ${contador}`);
+    contador++; // ++ incremento
+}
