20abd49192293241d0c1ac81151033ee03d7469d
llukezin <lucassmendonca77@gmail.com>
2023-10-17 17:26:16 -0300

    ATIVIDADE

--- js/06-funcoes.js
+++ js/06-funcoes.js
@@ -67,12 +67,3 @@ console.log(`Preço: ${formatarpreco(preco)}`);
 console.log(`desconto: ${formatarpreco(desconto)}`);
 
 console.log(`Preço final: ${formatarpreco(precoFinal)}`);
-
-/* Crie um arquivo chamado exercicio03.html.
-A partir dele, faça um script chamado exercicio03.js (não se esqueça de usar a pasta js).
-Programe no arquivo JS recursos que permitam converter um valor em dólar (ex: 1000 dólares) para um valor em reais (ex: 5.030 reais).
-Para esta atividade, você deverá criar uma função chamada converteMoeda e realizar dentro dela a operação de conversão.
-
-Dica: para converter, basta pegar o valor e multiplicar pela cotação do dólar (hoje 17/10/2023, está em R$ 5,03)
-
-Após a conversão, mostre o valor original em dólar e o valor convertido para reais. */
---
+++ js/exercicio03.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let valor= 1000
+let dolar= 5.03
+
+function converteMoeda(valor){
+   
+    return valor * dolar;
+}
+
+console.log(converteMoeda(valor));
