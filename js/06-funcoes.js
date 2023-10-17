42bee1d35f8905550a7b07d63b534aa8f4937982
llukezin <lucassmendonca77@gmail.com>
2023-10-17 15:31:43 -0300

    ponto nos numeros

--- js/06-funcoes.js
+++ js/06-funcoes.js
@@ -8,3 +8,62 @@ Usar funções também é benéfico do ponto de vista de organização de códig
 
 O JS já possui centenas de funções prontas:
 log(), alert(), prompt(), toFixed(), etc */
+
+/* como escrever funções no JS? */
+
+//  Forma 1: função anônima 
+const exemplo1 = function(){
+    console.log("Função anônima");
+};
+
+// Forma 2: funções nomeada/declarada
+function exemplo2(){
+    console.log("Função nomeada/declarada");
+};
+
+// Forma 3: arrow function
+const exemplo3 = () => {
+    console.log("Arrow Function");
+};
+
+// Chamada de função
+exemplo1();
+exemplo2();
+exemplo3(); 
+
+
+function somar(valor1, valor2){
+    return valor1 + valor2;
+}
+
+console.log(somar (10, 20) );
+console.log(somar (15, 50) );
+console.log(somar ( 30, 47.8) );
+
+/* Chamamos a função diversas vezes e passando valores diferentes como parâmetros. */
+
+console.log( somar(10,20));
+
+console.log( somar(10,20));
+
+console.log( somar(10,20));
+
+ 
+
+/* Formatação de valores em reais */
+
+ 
+
+let preco = 5500;
+
+let desconto = preco * 0.10; //
+
+let precoFinal = preco - desconto;
+
+ 
+
+console.log(`Preço: ${preco.toLocaleString()}`);
+
+console.log(`desconto: ${desconto.toLocaleString()}`);
+
+console.log(`Preço final: ${precoFinal.toLocaleString()}`);
