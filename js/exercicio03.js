5aa64eeb7395828407a6f58956873b997d823e9d
llukezin <lucassmendonca77@gmail.com>
2023-10-18 14:36:17 -0300

    correção do exercício

--- js/exercicio03.js
+++ js/exercicio03.js
@@ -1,11 +1,18 @@
 "use strict";
 
-let valor= 1000
-let dolar= 5.03
+// dados de entrada
+let valorDaCotacaoDoDia= 1000
+let valorEmDolares= 5.03
 
-function converteMoeda(valor){
-   
-    return valor * dolar;
+//Função que reaçiza a conversão matematica ao receber PARÂMETROS
+function converteMoeda(valor, cotacao ){
+    return valor * cotacao;
 }
 
-console.log(converteMoeda(valor));
+/* variavel que receberá o resultado processado pela função Depois que  
+a função terminar sua execução */
+let valorConvertido = converteMoeda(valorDaCotacaoDoDia, valorEmDolares);
+
+//saidas apos o processamento 
+console.log(valorEmDolares);// quanto que era em dolares 
+console.log(valorConvertido);// quanto que fica em reias
